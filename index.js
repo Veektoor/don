a64f6080bb5d9b30ea350a72d47ce5b458822f51
Veektoor <gichuhivictorm@gmail.com>
2024-05-07 14:51:00 +0300

    changes

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const http=require('http')
-const fs =require('fs')
+const http = require('http');
+const fs = require('fs');
 
+// Use process.env.PORT to get the port assigned by Heroku
 const port = process.env.PORT || 8000;
 
-const server=http.createServer((req, res)=>{
-    if(req.url=='/'){
-        fs.readFile(__dirname + '/index.html',(err, data)=>{
-            if(err){
+const server = http.createServer((req, res) => {
+    if (req.url == '/') {
+        fs.readFile(__dirname + '/index.html', (err, data) => {
+            if (err) {
                 res.writeHead(500);
                 res.end("error handling the page")
-    
-            }else{
-                res.writeHead(200,{'Content-Type':'text/html'});
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
                 res.end(data);
             }
-        })
+        });
     }
-})
-server.listen(8000,()=>{
-    console.log(`server is listening on port ${port}`)
-})
+});
+
+server.listen(port, () => {
+    console.log(`server is listening on port ${port}`);
+});
